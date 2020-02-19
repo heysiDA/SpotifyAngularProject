7c7697bc172434e7acde1e86e162d66e9e1da63a
heysi <heysidepaignea@gmail.com>
2020-02-19 13:44:41 -0300

    Creating the main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Router } from '@angular/router';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {
+
+  }
+
+ }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  countries:any[] =[];
+  constructor(private http: HttpClient) { 
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe((data:any)=>{
+      this.countries = data;
+      console.log(data);
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
